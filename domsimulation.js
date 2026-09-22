d9bd32f3f8d3378e1f5d5e03c9be4a73d6b54963
devmasand1447 <devmasand7@gmail.com>
2026-09-22 11:07:09 +0530

    Implement Button class with event handling

---
+++ domsimulation.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+
+class Button extends EventEmitter{}
+const button = new Button();
+button.on('click' , () => console.log('button clicked'));
+button.on('mouseover',()=>console.log('Mouse is over the button'));
+
+button.emit('click');
+button.emit('mouseover');
